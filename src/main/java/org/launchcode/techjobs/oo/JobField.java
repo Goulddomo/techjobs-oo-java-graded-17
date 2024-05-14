e60004175cee7815ac0ce3094b50bf59debe2e80
Dominique Gould <dominiquegould42@yahoo.com>
2024-05-13 19:46:30 -0500

    HELP

---
+++ src/main/java/org/launchcode/techjobs/oo/JobField.java
@@ -0,0 +1,63 @@
+package org.launchcode.techjobs.oo;
+
+import java.util.Objects;
+
+public abstract class JobField {
+    private int id;
+    private static int nextId = 1;
+    private String value;
+
+
+    public JobField() {
+        id = nextId;
+        nextId++;
+    }
+
+    // Constructor
+    public JobField(String value) {
+        this();
+        this.value = value;
+    }
+
+    //    Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+
+    //  Setters
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+
+    //    ToString
+    @Override
+    public String toString() {
+        return "JobField{" +
+                "id=" + id +
+                ", value='" + value + '\'' +
+                '}';
+    }
+
+
+    //    HashCode
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        JobField jobField = (JobField) o;
+        return id == jobField.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+
+}
